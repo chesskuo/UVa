d0aaa86e6079edae7fa2fa4af47c9c172b33fd26
Chess <gyc880323@gmail.com>
2018-09-02 13:11:03 +0800

    Update and rename 488 - Triangle Wave to 488 - Triangle Wave.cpp

--- 488 - Triangle Wave.cpp
+++ 488 - Triangle Wave.cpp
@@ -1,8 +1,9 @@
-/*******************************************************************************************************
-* UVa 488 - Triangle Wave                                                                              *
-* https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=429  *
-* https://www.udebug.com/UVa/488                                                                       *
-*******************************************************************************************************/
+// ----------
+// UVa 488 - Triangle Wave
+// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=429
+// https://www.udebug.com/UVa/488
+// ----------
+
 #include <cstdio>
 
 using namespace std;

bfc16d65f9bc4dbd53f87932e68456619533701e
Chess <gyc880323@gmail.com>
2018-09-02 13:07:27 +0800

    Update and rename 11988 - Broken Keyboard (a.k.a. Beiju Text) to 11988 - Broken Keyboard (a.k.a. Beiju Text).cpp

--- 11988 - Broken Keyboard (a.k.a. Beiju Text).cpp
+++ 11988 - Broken Keyboard (a.k.a. Beiju Text).cpp
@@ -1,8 +1,8 @@
-/*******************************************************************************************************
-* UVa 11988 - Broken Keyboard (a.k.a. Beiju Text)                                                      *
-* https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3139 *
-* https://www.udebug.com/UVa/11988                                                                     *
-*******************************************************************************************************/
+// ----------
+// UVa 11988 - Broken Keyboard (a.k.a. Beiju Text)
+// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3139
+// https://www.udebug.com/UVa/11988
+// ----------
 #include <cstdio>
 #include <cstring>
 #include <list>
